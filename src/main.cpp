9778ff742f8afed90d77040c0509ee5405d4a79e
Lizenzinhaber <christoph.willhalm@gmail.com>
2024-05-31 12:15:25 +0200

    Neuanfang 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,11 @@
-#include <Arduino.h>
-<<<<<<< HEAD
+#include "Arduino.h"
 
-void setup() {
-  Serial.begin(115200);
-}
-
-void loop() {
-  
-=======
-#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-#include <MQUnifiedsensor.h>
-
-void setup()
+void setup() 
 {
-  printf("Hello World");
+  Serial.begin(115200);
 }
 
 void loop()
 {
 
->>>>>>> 88f9f12efb1fe3dde097d28200217ff4ff856d67
 }
